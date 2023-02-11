9e437030b9ea1c8397db8d54174d1690966b3c13
TaichiEndo <endotaichi19951101america@gmail.com>
2023-02-11 12:51:58 -0500

    first hand commit

---
+++ Ios_version3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Ios_version3
+//
+//  Created by taichi endo on 2/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ios_version3/Ios_version3App.swift
@@ -0,0 +1,17 @@
+//
+//  Ios_version3App.swift
+//  Ios_version3
+//
+//  Created by taichi endo on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Ios_version3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
